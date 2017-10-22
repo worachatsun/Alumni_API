a08e0951428c31fb6fce2e12128c58728f038e1d
boszerwp <wachirawit.bosz@mail.kmutt.ac.th>
2017-10-22 16:42:28 +0000

    edit env & add Dockerfile

--- config/env/development.js
+++ config/env/development.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoUri: process.env.MONGO_HOST+process.env.MONGO_TABLE || 'mongodb://localhost/Alumni',
+    mongoUri: process.env.MONGO_HOST+process.env.MONGO_TABLE || 'mongodb://db/Alumni',
     debug: true
 }
